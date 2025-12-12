01ebaf96a4fe1acd85730b552d15b734fde6358f
hamzajafferdeveloper <hamzajaffer999@gmail.com>
2025-12-12 12:39:43 +0500

    Add Create Svg Template Page

--- resources/js/lib/utils.ts
+++ resources/js/lib/utils.ts
@@ -1,5 +1,6 @@
 import { InertiaLinkProps } from '@inertiajs/react';
 import { type ClassValue, clsx } from 'clsx';
+import Color from 'color';
 import { twMerge } from 'tailwind-merge';
 
 export function cn(...inputs: ClassValue[]) {
@@ -16,3 +17,27 @@ export function isSameUrl(
 export function resolveUrl(url: NonNullable<InertiaLinkProps['href']>): string {
     return typeof url === 'string' ? url : url.url;
 }
+
+export const extractFillMap = (svgText: string): Record<string, string> => {
+    const map: Record<string, string> = {};
+    const styleRegex = /\.([a-zA-Z0-9_-]+)\s*\{([^}]+)\}/g;
+    let match;
+
+    while ((match = styleRegex.exec(svgText)) !== null) {
+        const className = match[1];
+        const body = match[2];
+        const fillMatch = body.match(/fill\s*:\s*([^;]+);?/);
+
+        if (fillMatch) {
+            try {
+                const normalized = Color(fillMatch[1].trim())
+                    .hex()
+                    .toUpperCase();
+                map[className] = normalized;
+            } catch {
+                // skip invalid colors
+            }
+        }
+    }
+    return map;
+};
--- resources/js/pages/admin/product/svg-template/create.tsx
+++ resources/js/pages/admin/product/svg-template/create.tsx
@@ -1,9 +1,42 @@
-import React from 'react'
+import AppLayout from '@/layouts/app-layout';
+import admin from '@/routes/admin';
+import { BreadcrumbItem } from '@/types';
+import { Product } from '@/types/data';
+import { Head } from '@inertiajs/react';
+import AddTemplateSection from '@/sections/add-template-section';
 
-const CreateSvgTemplate = () => {
-  return (
-    <div>CreateSvgTemplate</div>
-  )
-}
 
-export default CreateSvgTemplate
+type Props = {
+    product: Product;
+};
+
+export type SelectedPart = {
+    id: string;
+    name?: string;
+    protection?: boolean;
+    isGroup?: boolean;
+    color?: string;
+    defaultColor?: string;
+};
+
+const CreateSvgTemplate = ({ product }: Props) => {
+    const breadcrumbs: BreadcrumbItem[] = [
+        {
+            title: 'All Product',
+            href: admin.product.index().url,
+        },
+        {
+            title: 'Create Svg Template',
+            href: admin.product.svgTemplate.create(product.slug).url,
+        },
+    ];
+
+    return (
+        <AppLayout breadcrumbs={breadcrumbs}>
+            <Head title="Add Svg Template" />
+            <AddTemplateSection />
+        </AppLayout>
+    );
+};
+
+export default CreateSvgTemplate;
